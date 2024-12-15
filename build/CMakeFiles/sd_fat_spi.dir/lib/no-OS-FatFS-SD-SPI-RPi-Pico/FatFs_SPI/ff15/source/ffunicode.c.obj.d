c9c647d19e7da3d9304a981187dc2845c20974a9
Tarek Benbrahim <tbr694@gmail.com>
2024-12-15 13:52:13 +0100

    Initial commit

---
+++ build/CMakeFiles/sd_fat_spi.dir/lib/no-OS-FatFS-SD-SPI-RPi-Pico/FatFs_SPI/ff15/source/ffunicode.c.obj.d
@@ -0,0 +1,5 @@
+CMakeFiles/sd_fat_spi.dir/lib/no-OS-FatFS-SD-SPI-RPi-Pico/FatFs_SPI/ff15/source/ffunicode.c.obj: \
+ /home/tarek/Desktop/digi/lib/no-OS-FatFS-SD-SPI-RPi-Pico/FatFs_SPI/ff15/source/ffunicode.c \
+ /home/tarek/Desktop/digi/lib/no-OS-FatFS-SD-SPI-RPi-Pico/FatFs_SPI/ff15/source/ff.h \
+ /home/tarek/Desktop/digi/lib/no-OS-FatFS-SD-SPI-RPi-Pico/FatFs_SPI/ff15/source/ffconf.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h
---
+++ build/CMakeFiles/sd_fat_spi.dir/lib/no-OS-FatFS-SD-SPI-RPi-Pico/FatFs_SPI/sd_driver/sd_spi.c.obj.d
@@ -0,0 +1,69 @@
+CMakeFiles/sd_fat_spi.dir/lib/no-OS-FatFS-SD-SPI-RPi-Pico/FatFs_SPI/sd_driver/sd_spi.c.obj: \
+ /home/tarek/Desktop/digi/lib/no-OS-FatFS-SD-SPI-RPi-Pico/FatFs_SPI/sd_driver/sd_spi.c \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /usr/include/newlib/stdio.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/newlib.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/config.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/sys/features.h /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdarg.h \
+ /usr/include/newlib/sys/reent.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/sys/lock.h /usr/include/newlib/sys/types.h \
+ /usr/include/newlib/sys/_stdint.h /usr/include/newlib/machine/endian.h \
+ /usr/include/newlib/machine/_endian.h /usr/include/newlib/sys/select.h \
+ /usr/include/newlib/sys/_sigset.h /usr/include/newlib/sys/_timeval.h \
+ /usr/include/newlib/sys/timespec.h /usr/include/newlib/sys/_timespec.h \
+ /usr/include/newlib/sys/_pthreadtypes.h /usr/include/newlib/sys/sched.h \
+ /usr/include/newlib/machine/types.h /usr/include/newlib/sys/stdio.h \
+ /usr/include/newlib/string.h /usr/include/newlib/sys/_locale.h \
+ /usr/include/newlib/strings.h /usr/include/newlib/sys/string.h \
+ /home/tarek/pico-sdk/src/rp2_common/hardware_gpio/include/hardware/gpio.h \
+ /home/tarek/pico-sdk/src/common/pico_base/include/pico.h \
+ /home/tarek/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /home/tarek/pico-sdk/src/common/pico_base/include/pico/assert.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h \
+ /usr/include/newlib/assert.h \
+ /home/tarek/Desktop/digi/build/generated/pico_base/pico/version.h \
+ /home/tarek/pico-sdk/src/common/pico_base/include/pico/config.h \
+ /home/tarek/Desktop/digi/build/generated/pico_base/pico/config_autogen.h \
+ /home/tarek/pico-sdk/src/boards/include/boards/pico.h \
+ /home/tarek/pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h \
+ /home/tarek/pico-sdk/src/rp2_common/pico_platform/include/pico/platform.h \
+ /home/tarek/pico-sdk/src/rp2040/hardware_regs/include/hardware/platform_defs.h \
+ /home/tarek/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/addressmap.h \
+ /home/tarek/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/sio.h \
+ /home/tarek/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /home/tarek/pico-sdk/src/common/pico_base/include/pico/error.h \
+ /home/tarek/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/sio.h \
+ /home/tarek/pico-sdk/src/rp2_common/hardware_base/include/hardware/address_mapped.h \
+ /home/tarek/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/interp.h \
+ /home/tarek/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/padsbank0.h \
+ /home/tarek/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/pads_bank0.h \
+ /home/tarek/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/iobank0.h \
+ /home/tarek/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/io_bank0.h \
+ /home/tarek/pico-sdk/src/rp2_common/hardware_irq/include/hardware/irq.h \
+ /home/tarek/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/intctrl.h \
+ /home/tarek/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/m0plus.h \
+ /home/tarek/Desktop/digi/lib/no-OS-FatFS-SD-SPI-RPi-Pico/FatFs_SPI/include/my_debug.h \
+ /home/tarek/Desktop/digi/lib/no-OS-FatFS-SD-SPI-RPi-Pico/FatFs_SPI/sd_driver/sd_card.h \
+ /home/tarek/pico-sdk/src/common/pico_sync/include/pico/mutex.h \
+ /home/tarek/pico-sdk/src/common/pico_sync/include/pico/lock_core.h \
+ /home/tarek/pico-sdk/src/common/pico_time/include/pico/time.h \
+ /home/tarek/pico-sdk/src/rp2_common/hardware_timer/include/hardware/timer.h \
+ /home/tarek/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/timer.h \
+ /home/tarek/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/timer.h \
+ /home/tarek/pico-sdk/src/rp2_common/hardware_sync/include/hardware/sync.h \
+ /home/tarek/Desktop/digi/lib/no-OS-FatFS-SD-SPI-RPi-Pico/FatFs_SPI/ff15/source/ff.h \
+ /home/tarek/Desktop/digi/lib/no-OS-FatFS-SD-SPI-RPi-Pico/FatFs_SPI/ff15/source/ffconf.h \
+ /home/tarek/Desktop/digi/lib/no-OS-FatFS-SD-SPI-RPi-Pico/FatFs_SPI/sd_driver/spi.h \
+ /home/tarek/pico-sdk/src/rp2_common/hardware_dma/include/hardware/dma.h \
+ /home/tarek/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/dma.h \
+ /home/tarek/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/dma.h \
+ /home/tarek/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/dreq.h \
+ /home/tarek/pico-sdk/src/rp2_common/hardware_spi/include/hardware/spi.h \
+ /home/tarek/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/spi.h \
+ /home/tarek/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/spi.h \
+ /home/tarek/pico-sdk/src/common/pico_sync/include/pico/sem.h \
+ /home/tarek/Desktop/digi/lib/no-OS-FatFS-SD-SPI-RPi-Pico/FatFs_SPI/sd_driver/sd_spi.h
